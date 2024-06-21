0f0fb979936768b4850498ab069196fc4735ad64
Dominik M <phoenix.dh-m16@outlook.com>
2024-06-21 17:03:19 +0200

    Files have been revised

--- www/assets/js/dataprotection.js
+++ www/assets/js/dataprotection.js
@@ -2,58 +2,83 @@
 
 const ID_privacy_policy = document.getElementById('privacy_policy');
 const ID_legal_notice = document.getElementById('legal_notice');
+
+const CLASS_help = document.querySelector('.help');
 const CLASS_privacyPolicy = document.querySelector('.privacyPolicy');
 const CLASS_legalNotice = document.querySelector('.legalNotice');
 
+/**
+ * Initialisiert die Seite, indem die aktuelle Seite überprüft und entsprechende Aktionen ausgeführt werden.
+ */
 function init() {
     checkTheCurrentPage();
 }
 
+/**
+ * Überprüft die aktuelle URL, um festzustellen, welche Seite angezeigt werden soll, 
+ * und ruft die entsprechende Funktion auf, um die Seite zu laden.
+ */
 function checkTheCurrentPage() {
     const currentPageUrl = window.location.href;
-    if (currentPageUrl.includes('privacy_policy')) {
-        currentDataprotectionPage('privacy_policy');
-        return;
-    }
-    if (currentPageUrl.includes('legal_notice')) {
-        currentDataprotectionPage('legal_notice');
-        return;
-    }
-    console.error('Etwas stimmt nicht mit der URL:', currentPageUrl);
+    currentPageUrl.includes('privacy_policy') && currentDataprotectionPage('privacy_policy');
+    currentPageUrl.includes('legal_notice') && currentDataprotectionPage('legal_notice');
 }
 
+/**
+ * Lädt die angeforderte Seite basierend auf dem übergebenen Seitennamen 
+ * und führt die entsprechenden Aktionen zur Aktualisierung der Seite durch.
+ * 
+ * @param {string} currentPage - Der Name der aktuellen Seite (z.B. 'privacy_policy', 'legal_notice', 'help').
+ */
 function currentDataprotectionPage(currentPage) {
-    if (currentPage === 'privacy_policy') {
-        currentPagePrivacyPolicy();
-    }
-    if (currentPage === 'legal_notice') {
-        currentPageLegalNotice();
-    }
+    currentPage === 'help' && currentPageHelp();
+    currentPage === 'privacy_policy' && currentPagePrivacyPolicy();
+    currentPage === 'legal_notice' && currentPageLegalNotice();
 }
 
+/**
+ * Führt die notwendigen Aktionen aus, um die "Privacy Policy"-Seite anzuzeigen.
+ */
 function currentPagePrivacyPolicy() {
-    if (ID_legal_notice.classList.contains('currentPage')) {
-        ID_legal_notice.classList.remove('currentPage');
-        CLASS_legalNotice.classList.remove('activePage');
-        ID_privacy_policy.classList.remove('nextPage');
-    }
+    ID_legal_notice.classList.contains('currentPage') && removeLegalNoticeClass();
     ID_legal_notice.classList.add('nextPage');
     ID_privacy_policy.classList.add('currentPage');
     CLASS_privacyPolicy.classList.add('activePage');
 }
 
+/**
+ * Führt die notwendigen Aktionen aus, um die "Legal Notice"-Seite anzuzeigen.
+ */
 function currentPageLegalNotice() {
-    if (ID_privacy_policy.classList.contains('currentPage')) {
-        ID_privacy_policy.classList.remove('currentPage');
-        CLASS_privacyPolicy.classList.remove('activePage');
-        ID_legal_notice.classList.remove('nextPage');
-    }
+    ID_privacy_policy.classList.contains('currentPage') && removePrivacyPolicyClass();
     ID_privacy_policy.classList.add('nextPage');
     ID_legal_notice.classList.add('currentPage');
     CLASS_legalNotice.classList.add('activePage');
 }
 
+/**
+ * Entfernt die Klassen der "Legal Notice"-Seite, um deren Anzeige zu beenden.
+ */
+function removeLegalNoticeClass() {
+    ID_legal_notice.classList.remove('currentPage');
+    CLASS_legalNotice.classList.remove('activePage');
+    ID_privacy_policy.classList.remove('nextPage');
+}
+
+/**
+ * Entfernt die Klassen der "Privacy Policy"-Seite, um deren Anzeige zu beenden.
+ */
+function removePrivacyPolicyClass() {
+    ID_privacy_policy.classList.remove('currentPage');
+    CLASS_privacyPolicy.classList.remove('activePage');
+    ID_legal_notice.classList.remove('nextPage');
+}
+
+/**
+ * Führt eine Aktion aus, um zur vorherigen Seite zurückzukehren oder das Fenster zu schließen.
+ */
 function actionBack() {
     // window.history.back();
-    window.close();
+    // window.close();
+    console.warn('noch in arbeit!');
 }
