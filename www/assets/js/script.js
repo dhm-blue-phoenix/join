413554eca163ebbced5297443969f0df5f40774a
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2024-10-24 19:30:23 +0200

    js

--- www/assets/js/script.js
+++ www/assets/js/script.js
@@ -194,6 +194,15 @@ function btnsnavbar() {
     btns[0].className += " active";
 };
 
+/**
+ * Aktualisiert den Textinhalt des Elements mit der ID "massage",
+ * um einen Gruß basierend auf der aktuellen Tageszeit anzuzeigen.
+ *
+ * Diese Funktion überprüft die aktuelle Stunde und setzt den Gruß
+ * auf "Guten Morgen," für Stunden zwischen 0 und 11, "Guten Tag,"
+ * für Stunden zwischen 12 und 17 und "Guten Abend," für Stunden
+ * zwischen 18 und 23.
+ */
 function showGreeting() {
     const currentHour = new Date().getHours();
     const massage = document.getElementById("massage");
