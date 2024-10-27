971800e8d2c5dd328bb98662162c0a004b790b22
Maximilian Wagener <159955254+DeVAiRMaX@users.noreply.github.com>
2024-10-27 11:05:00 +0100

    JSDoc

--- www/assets/js/initSummary.js
+++ www/assets/js/initSummary.js
@@ -52,16 +52,14 @@ async function loadTaskStatusPrio() {
 }
 
 /**
- * Läd die Task-Status-Daten von der Datenbank.
- * Die Daten werden in einer Array-Struktur gespeichert, die die folgenden Eigenschaften enthält:
- * - count: Die Anzahl der Tasks mit dem jeweiligen Status
- * - prio: Der Prio-Wert des Status (low, medium, urgent)
- * - text: Der Text des Status (z.B. "zu erledigen")
- * - value: Der Wert des Status (0, 1, 2 oder 3)
- * Die Funktion gibt ein Promise zur ck, das die gefundene Task-Status-Daten enth lt.
- * @returns {Promise<Object>} - Ein Promise mit den gefundenen Task-Status-Daten
+ * Läd die Anzahl der Tasks pro Kategorie von der Datenbank.
+ * Die Anzahl wird in einem Array mit vier Elementen und der Gesamtanzahl
+ * der Tasks in einer extra Property 'totalTaskCount' im zurückgegebenen
+ * Objekt zurückgegeben.
+ * @returns {Promise<{taskStatusData: {count: number}[], totalTaskCount: number}>} - Ein Promise, das die Anzahl der Tasks pro Kategorie und die Gesamtanzahl der Tasks zurück gibt.
  * @throws {Error} Wenn ein Fehler bei der Daten bertragung auftritt.
  */
+
 async function loadTaskStatusData() {
   try {
     let totalTaskCount = 0;
@@ -79,6 +77,13 @@ async function loadTaskStatusData() {
     console.error(`Ein schwerwiegender Fehler ist beim Rendern aufgetreten! ${err}`);
   };
 }
+
+/**
+ * Initialisiert die Anzeige der Task-Statistik auf der Summary-Seite.
+ * Läd die Anzahl der Tasks pro Kategorie und die Anzahl der Tasks mit Prio 'urgent' von der Datenbank.
+ * Füllt die Container mit den ermittelten Anzahlen.
+ * @returns {Promise<void>}
+ */
 
 async function init() {
   const urgentTaskCount = await loadTaskStatusPrio();
